ee75e32f613914a090621900afe9bb977bc36b3c
Alek <themain89@mail.ru>
2023-02-01 00:50:45 +0300

    max sale added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       SalesManager salesManager = new SalesManager(new int[] {15, 100, 500, 150});
+       int maxSale = salesManager.max();
+        System.out.println(maxSale);
+
+
+
+
+
+        // System.out.println("Hello world!");
     }
 }
